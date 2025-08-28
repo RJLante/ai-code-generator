dfda270c9768855aa36ccb01b62f1b42cda33c34
RJLante <rujiang@tcd.ie>
2025-08-28 21:05:41 +0800

    user module

--- src/main/java/com/rd/aicodegenerator/AiCodeGeneratorApplication.java
+++ src/main/java/com/rd/aicodegenerator/AiCodeGeneratorApplication.java
@@ -1,11 +1,12 @@
 package com.rd.aicodegenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.rd.aicodegenerator.mapper")
 public class AiCodeGeneratorApplication {
 
     public static void main(String[] args) {
