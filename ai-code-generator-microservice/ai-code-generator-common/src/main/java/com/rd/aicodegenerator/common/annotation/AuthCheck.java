01aa74d3179702bad7f1b9c5aba35d72a5ede87e
RJLante <rujiang@tcd.ie>
2025-10-01 11:41:57 +0800

    microservice - common

--- ai-code-generator-microservice/ai-code-generator-common/src/main/java/com/rd/aicodegenerator/common/annotation/AuthCheck.java
+++ ai-code-generator-microservice/ai-code-generator-common/src/main/java/com/rd/aicodegenerator/common/annotation/AuthCheck.java
@@ -1,6 +1,6 @@
 package com.rd.aicodegenerator.common.annotation;
 
-import java.lang.annotation.ElementType;
+import  java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
